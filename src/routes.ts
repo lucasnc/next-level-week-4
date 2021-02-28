3ac77bf53d334270bc3884882127368f33015b28
lucasnc <lucascdna@gmail.com>
2021-02-28 14:29:07 -0300

    NPS calculator & validations

--- src/routes.ts
+++ src/routes.ts
@@ -1,4 +1,6 @@
 import { Router } from 'express';
+import { AnswerController } from './controllers/AnswerController';
+import { NpsController } from './controllers/NpsController';
 import { SendMailController } from './controllers/SendMailController';
 import { SurveysController } from './controllers/SurveysController';
 import { UserController } from './controllers/UserController';
@@ -9,6 +11,9 @@ const userController = new UserController();
 const surveyController = new SurveysController();
 
 const sendMailController = new SendMailController();
+const answerController = new AnswerController();
+
+const npsController = new NpsController();
 
 router.post("/users", userController.create);
 router.post("/surveys", surveyController.create);
@@ -17,5 +22,9 @@ router.get("/users", userController.show);
 router.get("/surveys", surveyController.show);
 
 router.post("/sendMail", sendMailController.execute);
+
+router.get("/answers/:value", answerController.execute);
+
+router.get("/nps/:survey_id", npsController.execute);
 
 export { router };
